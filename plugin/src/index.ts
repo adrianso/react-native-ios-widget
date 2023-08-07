791d191828b7a38d7efae8c7cb6fdd16472d8c57
Adrian So <adrian.so@foodstuffs.co.nz>
2023-08-08 09:38:29 +1200

    Remove unused attributes

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -5,7 +5,6 @@ import { withPodfile } from "./withPodfile";
 import { withConfig } from "./withConfig";
 
 const withWidget: ConfigPlugin<{
-  frequentUpdates?: boolean;
   widgetsFolder?: string;
   deploymentTarget?: string;
   groupIdentifier?: string;
@@ -14,7 +13,6 @@ const withWidget: ConfigPlugin<{
 }> = (
   config,
   {
-    frequentUpdates,
     deploymentTarget = "14.0",
     widgetsFolder = "widgets",
     groupIdentifier,
@@ -23,15 +21,6 @@ const withWidget: ConfigPlugin<{
 ) => {
   const targetName = "WidgetsExtension";
   const bundleIdentifier = `${config.ios?.bundleIdentifier}.Widgets`;
-
-  config.ios = {
-    ...config.ios,
-    infoPlist: {
-      ...config.ios?.infoPlist,
-      NSSupportsLiveActivities: true,
-      NSSupportsLiveActivitiesFrequentUpdates: frequentUpdates,
-    },
-  };
 
   return withPlugins(config, [
     [
