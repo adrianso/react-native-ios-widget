a3b1bced30adc1797c6b98a2cb84e91a43cded07
Adrian So <adrian@sld.co.nz>
2023-09-11 15:42:24 +1200

    Move widget block below Expo stuff

--- plugin/src/withPodfile.ts
+++ plugin/src/withPodfile.ts
@@ -22,30 +22,30 @@ export const withPodfile: ConfigPlugin<Required<WidgetConfig>> = (
       let podFileContent = fs.readFileSync(podFilePath).toString();
 
       podFileContent = mergeContents({
-        tag: "react-native-widget-extension-1",
+        tag: "react-native-ios-widget",
         src: podFileContent,
-        newSrc: `installer.pods_project.targets.each do |target|
-          target.build_configurations.each do |config|
-            config.build_settings['APPLICATION_EXTENSION_API_ONLY'] = 'No'
-          end
-        end`,
+        newSrc: `\t\tinstaller.pods_project.targets.each do |target|
+\t\t\ttarget.build_configurations.each do |config|
+\t\t\t\tconfig.build_settings['APPLICATION_EXTENSION_API_ONLY'] = 'No'
+\t\t\tend
+\t\tend`,
         anchor:
           /installer.target_installation_results.pod_target_installation_results/,
-        offset: 0,
-        comment: "#",
+        offset: 8,
+        comment: "\t\t#",
       }).contents;
 
       podFileContent = podFileContent
-        .concat(`\n\n# >>> Inserted by react-native-widget-extension\n`)
+        .concat(`\n\n# >>> Inserted by react-native-ios-widget\n`)
         .concat(
           `target '${targetName}' do
-            use_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
-            use_frameworks! :linkage => ENV['USE_FRAMEWORKS'].to_sym if ENV['USE_FRAMEWORKS']
+\tuse_frameworks! :linkage => podfile_properties['ios.useFrameworks'].to_sym if podfile_properties['ios.useFrameworks']
+\tuse_frameworks! :linkage => ENV['USE_FRAMEWORKS'].to_sym if ENV['USE_FRAMEWORKS']
 
-            ${pods.map((pod) => `pod '${pod}'`).join("\n")}
-          end`
+  ${pods.map((pod) => `\tpod '${pod}'`).join("\n")}
+end`
         )
-        .concat(`\n# >>> Inserted by react-native-widget-extension`);
+        .concat(`\n# >>> Inserted by react-native-ios-widget`);
 
       fs.writeFileSync(podFilePath, podFileContent);
 
